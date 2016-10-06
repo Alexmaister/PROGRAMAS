7fea2a6b455dc0a0037b02ce3d70a52e3f3f497e
Alexmaister <aortizurita@gmail.com>
2016-10-06 02:17:37 +0200

    analisis

---
+++ PROGRAMAS/Tema3/Ejercicios/CilindroAreaVolumen.java
@@ -0,0 +1,38 @@
+package Ejercicios;
+
+/*
+ * ANALISIS:
+ * Programa para calcular el área lateral, total y el volumen del cilindro cuyos datos se leen por teclado.
+ * 
+ * REQUISITOS:
+ * Calcular el area lateral total y volumen de un cilindro
+ * 
+ * ENTRADAS:
+ * Radio y altura del cilindro
+ * 
+ * SALIDAS:
+ * Se mostrara por pantalla el area lateral , total y el volumen del cilindro
+ * 
+ * PRECONDICIONES:
+ * El radio y la altura pueden ser enteros o reales
+ * 
+ * POSTCONDICIONES
+ * Se mostraran en pantalla los resultados
+ * 
+ * RESTRICCIONES:
+ * Ninguna
+ * 
+ * SUPOSICIONES:
+ * Supondremos que los datos introducidos seran numeros
+ * 
+ * */
+
+public class CilindroAreaVolumen {
+	
+	public static void main (String[]args){
+		
+		
+		
+	}
+
+}

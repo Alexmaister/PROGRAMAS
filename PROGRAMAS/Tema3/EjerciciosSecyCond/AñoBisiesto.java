3eb09ab41d6f28c7f10421d4605031760d557018
Alexmaister <aortizurita@gmail.com>
2016-10-07 10:47:19 +0200

    TERMINADO PSEUDOCODIGO AÑOBISIESTO

---
+++ PROGRAMAS/Tema3/EjerciciosSecyCond/AñoBisiesto.java
@@ -0,0 +1,42 @@
+package EjerciciosSecyCond;
+/*ANALISIS:
+ * Programa que leera un año de teclado y
+ *  nos dira si es o no un año bisiesto
+ * 
+ * REQUISITOS:
+ * Calcular si un año es bisiesto
+ * 
+ * ENTRADA:
+ * Un ano en formato XXXX	
+ * 
+ * SALIDA:
+ * Mensaje si es bisiesto o no
+ * 
+ * RESTRICCIONES:
+ * El año no puede ser un numero negativo ni cero
+ * 
+ * SUPOSICIONES:
+ * Ninguna
+ * */
+/*
+ * PSEUDOCODIGO:
+ * 
+ * INICIO
+ * 	MOSTRAR MENSAJE
+ * 	LEER DATOS
+ * 	ECO DE LOS DATOS DE ENTRADA
+ * 	SI(AÑO>0)
+ * 		SI((AÑO%400==0) || ( (AÑO%4==0) && (AÑO%100 ==0))
+ * 			MOSTRAR AÑO BISIESTO
+ * 		SINO
+ * 			MOSTRAR AÑO NO BISIESTO
+ * 		FIN SINO
+ * 	SINO 
+ * 		MOSTRAR MENSAJE DE ERROR
+ * FIN SINO
+ * FIN
+ * */
+public class AñoBisiesto {
+
+	
+}

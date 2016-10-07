8d7918630cb8a9f131514ce20897362dee28f61c
Alexmaister <aortizurita@gmail.com>
2016-10-07 09:22:45 +0200

    PSEUDOCODIGO TIPOTRIANGULO

---
+++ PROGRAMAS/Tema3/EjerciciosSecyCond/TipoTriangulo.java
@@ -0,0 +1,50 @@
+package EjerciciosSecyCond;
+/*ANALISIS:
+ * Programa que leera por teclado los tres lados de un triangulo,
+ * e indicara de que tipo de triangulo se trata
+ * 
+ * REQUISITOS:
+ * Saber que tipo de triangulo es segun los lados
+ * 
+ * ENTRADAS:
+ * los lados del triangulo
+ * 
+ * SALIDAS:
+ * Tipo del triangulo
+ * 
+ * RESTRICCIONES:
+ * Ningun lado puede ser 0
+ * 
+ * SUPOSICIONES:
+ * Ninguna
+ * 
+ * */
+
+/*PSEUDOCODIGO
+ * 
+ * INICIO
+ * 	
+ * 	LEER DATOS 
+ * 	ECO DE LOS DATOS
+ * 		SI(ALGUN LADO =0)
+ * 			MOSTRAR MENSAJE
+ * 		SINO SI(A>B+C)
+ * 			MOSTRAR MENSAJE
+ * 		SINO
+ * 			SI(A*A=B*B+C*C)
+ * 				MOSTRAR TRIANGULO RECTANGULO
+ * 			SINO SI(A*A<B*B+C*C)
+ * 				MOSTRAR TRIANGULO ACUTANGULO
+ * 			SINO
+ * 				MOSTRAR TRIANGULO OBTUSANGULO
+ * 			FIN SINO
+ * 		FIN SINO
+ * 
+ * 
+ * 
+ * FIN
+ * 
+ * */
+public class TipoTriangulo {
+
+}

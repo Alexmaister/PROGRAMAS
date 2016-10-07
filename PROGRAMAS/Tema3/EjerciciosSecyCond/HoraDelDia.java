266714f659e0f2cc00b4214a12f187a71e72459b
Alexmaister <aortizurita@gmail.com>
2016-10-07 10:18:56 +0200

    TERMINADO ANALISIS

--- PROGRAMAS/Tema3/EjerciciosSecyCond/HoraDelDia.java
+++ PROGRAMAS/Tema3/EjerciciosSecyCond/HoraDelDia.java
@@ -1,5 +1,36 @@
 package EjerciciosSecyCond;
+/*ANALISIS:
+ * Programa que recibira una hora del dia y respondera con un mensaje
+ * segun sea por la mañana , tarde o noche
+ * 
+ * REQUISITOS:
+ * Saber momento del dia segun la hora
+ * 
+ * ENTRADAS:
+ * Hora en formato 24h
+ * 
+ * SALIDAS:
+ * Mensaje dependiendo de la hora
+ * 
+ * RESTRICCIONES:
+ * La hora debe estar entre las 00:00 y las 23:59
+ * 
+ * SUPOSICIONES:
+ * Supondremos que los datos introducidos son enteros 
+ * */
 
+/*PSEUDOCODIGO:
+ * 
+ * INICIO
+ * 	
+ * 	MOSTRAR MENSAJE
+ * 	LEER DATOS 
+ * 	ECO DE LOS DATOS
+ * 	SI (HORA NO ESTA ENTRE 00:00 
+ * 
+ * FIN
+ * 
+ * */
 public class HoraDelDia {
 
 }

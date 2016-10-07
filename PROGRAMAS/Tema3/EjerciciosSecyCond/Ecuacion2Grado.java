d0cb07180d24240ab80cdd5b52e56264fb6afe1c
Alexmaister <aortizurita@gmail.com>
2016-10-07 09:09:47 +0200

    PSEUDOCODIGO ECUACION DE SEGUNDO GRADO

---
+++ PROGRAMAS/Tema3/EjerciciosSecyCond/Ecuacion2Grado.java
@@ -0,0 +1,41 @@
+package EjerciciosSecyCond;
+/*ANALISIS:
+ * Programa que calculara una ecuacion de segundo grado
+ * 
+ * REQUISITOS:
+ * Calcular ecuacion de segundo grado
+ * 
+ * ENTRADAS:
+ * Datos de la ecuacion
+ * 
+ * SALIDAS:
+ * Resultado
+ * 
+ * RESTRICCIONES:
+ * Ninguna
+ * 
+ * SUPOSICIONES:
+ * Ninguna
+ * 
+ * */
+
+/*PSEUDOCODIGO:
+ * 
+ * INICIO
+ * 	LEER DATOS
+ * 	ECO DE LOS DATOS DE ENTRADA
+ * 		SI(PRIMER COEFICOENTE=0)
+ * 			MOSTRAR MENSAJE
+ * 		SINO
+ * 			SI (DENOMINADOR<0)
+ * 				MOSTRAR MENSAJE
+ * 			SINO
+ * 				CALCULAR ECUACION DE SEGUNDO GRADO
+ * 				MOSTRAR RESULTADO
+ * 			FIN SINO
+ * 		FIN SINO
+ * FIN
+ * */
+public class Ecuacion2Grado {
+
+}
---
+++ PROGRAMAS/Tema3/EjerciciosSecyCond/TrianguloSemiperimetro.java
@@ -0,0 +1,41 @@
+package EjerciciosSecyCond;
+import java.util.*;
+
+/*ANALISIS:
+ * Programa que calculara el area de un triangulo mediante su semiperimetro 
+ * calculado por los lados de este 
+ * 
+ * REQUISITOS:
+ * Calcular semiperimetro
+ * Calcular area
+ * 
+ * ENTRADAS:
+ * Lados del triangulo
+ * 
+ * SALIDAS:
+ * Area
+ * 
+ * RESTRICCIONES:
+ * Ningun lado puede ser igual a 0
+ * 
+ * SUPOSICIONES:
+ * Supondremos que los numeros con decimales estaran compuestos
+ * por una coma y no un punto
+ * */
+
+/*PSEUDOCODIGO:
+ * 
+ * INICIO
+ * 
+ * 	LEER DATOS 
+ * 	ECO DE LOS DATOS DE ENTRADA
+ * 	CALCULAR SEMIPARIMETRO
+ * 	CALCULAR AREA
+ * 	MOSTRAR RESULTADO 
+ * 
+ * FIN
+ * 
+ * */
+public class TrianguloSemiperimetro {
+
+}

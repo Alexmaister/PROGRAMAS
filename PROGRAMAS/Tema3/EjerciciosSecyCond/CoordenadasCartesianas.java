a8430fdf938478ace649b1ad05a08bc2dbadf944
Alexmaister <aortizurita@gmail.com>
2016-10-10 13:14:59 +0200

    creado cartesiano cambiar analisis y pseudocodigo

---
+++ PROGRAMAS/Tema3/EjerciciosSecyCond/CoordenadasCartesianas.java
@@ -0,0 +1,43 @@
+package EjerciciosSecyCond;
+/*ANALISIS:
+ * Programa que leera de teclado las coordenadas cartesianas (x,y,z)
+ * y nos dira en que octante se encuentra el punto descrito por las coordenadas
+ * 
+ * REQUISITOS:
+ * Determinar un cuadrante con tres coordenadas dadas
+ * 
+ * ENTRADAS:
+ * Coordenadas (x , y , z)
+ * 
+ * SALIDAS:
+ * Se mostrara por pantalla el octante al que pertenece el punto
+ * 
+ * RESTRICCIONES:
+ * Ninguna
+ * 
+ * SUPOSICIONES:
+ * Ninguna
+ * */
+
+/*PSEUDOCODIGO:
+ * 
+ * INICIO
+ * 
+ * 	LEER DATOS 
+ * 	ECO DE LOS DATOS DE ENTRADA
+ * 		SI (X!=0 && Y!=0 && Z!=0)
+ * 			
+ * 			SI(X==0)
+ * 				SI()
+ * 				MOSTRAR MENSAJE
+ * 			SINO SI(Y==0)
+ * 				MOSTRAR MENSAJE
+ * 			SINO SI(Z==0)
+ * 				MOSTRAR MENSAJE
+ * 			
+ * 
+ * FIN
+ * */
+public class CoordenadasCartesianas {
+
+}

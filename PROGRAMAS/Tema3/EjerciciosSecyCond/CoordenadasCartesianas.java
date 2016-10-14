505911e2197f10a8cec1feecb41df404b8fb74c2
Alexmaister <aortizurita@gmail.com>
2016-10-14 08:56:41 +0200

    TERMINADO PSEUDOCODIGO COORDENADAS CARTESIANAS

--- PROGRAMAS/Tema3/EjerciciosSecyCond/CoordenadasCartesianas.java
+++ PROGRAMAS/Tema3/EjerciciosSecyCond/CoordenadasCartesianas.java
@@ -4,7 +4,7 @@ package EjerciciosSecyCond;
  * y nos dira en que octante se encuentra el punto descrito por las coordenadas
  * 
  * REQUISITOS:
- * Determinar un cuadrante con tres coordenadas dadas
+ * Determinar un octante con tres coordenadas dadas
  * 
  * ENTRADAS:
  * Coordenadas (x , y , z)
@@ -28,13 +28,38 @@ package EjerciciosSecyCond;
  * 		SI (X!=0 && Y!=0 && Z!=0)
  * 			
  * 			SI(X==0)
- * 				SI()
- * 				MOSTRAR MENSAJE
+ * 				SI(Y>0 && Z>0)
+ * 					MOSTRAR MENSAJE
+ * 				SINO SI(Y<0 && Z<0)
+ * 					MOSTRAR MENSAJE
+ * 				SINO SI(Y<0 && Z>0)
+ * 					MOSTRAR MENSAJE
+ * 				SINO
+ * 					MOSTRAR MENSAJE
+ * 				FIN SI
+ * 
  * 			SINO SI(Y==0)
- * 				MOSTRAR MENSAJE
+ * 				SI(X>0 && Z>0)
+ * 					MOSTRAR MENSAJE
+ * 				SINO SI(X<0 && Z<0)
+ * 					MOSTRAR MENSAJE
+ * 				SINO SI(X<0 && Z>0)
+ * 					MOSTRAR MENSAJE
+ * 				SINO
+ * 					MOSTRAR MENSAJE
+ * 				FIN SI
  * 			SINO SI(Z==0)
- * 				MOSTRAR MENSAJE
- * 			
+ * 				SI(Y>0 && X>0)
+ * 					MOSTRAR MENSAJE
+ * 				SINO SI(Y<0 && X<0)
+ * 					MOSTRAR MENSAJE
+ * 				SINO SI(Y<0 && X>0)
+ * 					MOSTRAR MENSAJE
+ * 				SINO
+ * 					MOSTRAR MENSAJE
+ * 				FIN SI
+ * 		SINO
+ * 			MOSTRAR MENSAJE	
  * 
  * FIN
  * */

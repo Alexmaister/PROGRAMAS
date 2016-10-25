e3a0676adadb45cac05fcc085e23e90564211a43
Alexmaister <aortizurita@gmail.com>
2016-10-25 12:20:20 +0200

    Terminado notaMediaAltaBajayAlumnos

--- PROGRAMAS/Tema3/EjerciciosIterativos/NotaMediaAltayAlumnos.java
+++ PROGRAMAS/Tema3/EjerciciosIterativos/NotaMediaAltayAlumnos.java
@@ -1,12 +1,32 @@
 package EjerciciosIterativos;
 
+import java.util.Scanner;
+
 /*ANALISIS:
  * Programa que leera de teclado todas las notas de los examenes de una clase
  * hasta que una de esta sea negativa, y se calculara la nota media ,
  * la mas alta , la mas baja y el numero de alumnos del curso
  * 
  * BUCLES:
+ * (1)(2)VALIDAR ENTRADA (WHILE)
+ * VCB: CENTINELA 
+ * VALORES DEL CENTINELA: S O N
+ * LECTURA ANTICIPADA ANTES DE ENTRAR EN EL BUCLE
+ * ACTUALIZACION AL FINAL DE LA PRIMERA ITERACCION pior lectura final
+ * CONDICION DE SALIDA: QUE EL CENTINELA SEA IGUAL A S O N
  * 
+ * (3)VALIDAR DATO (WHILE)
+ * VCB:CENTINELA
+ * VALORES DEL CENTINELA DE -1 A 10
+ * LECTURA ANTICIPADA ANTES DE LA PRIMERA ITERACCION
+ * ACTUALIZACION AL FINAL DE LA PRIMERA ITERACCION por lectura final
+ * CONDICION DE SALIDA: QUE EL NUMERO ESTE ENTRE LOS DATOS DEL CENTINELA(>=-1 Y <=10)
+ * 
+ * (4)MIENTRAS DATO NO SEA SALIR(WHILE)
+ * VCB:CENTINELA VALOR -1
+ * LECTURA ANTICIPADA ANTES DE LA PRIMERA ITERACCION
+ * ACTUALIZACION ANTES DE LA SEGUNDA ITERACCION POR LECTURA INICIAL
+ * CONDICION DE SALIDA: DATO =-1
  * 
  * REQUISITOS:
  * hacer media de todas las notas
@@ -36,8 +56,8 @@ package EjerciciosIterativos;
  * 	MIENTRAS EJECUTAR SEA SI	(2)	
  * 		LEER Y VALIDAR DATO		(3)
  * 		MIENTRAS DATO NO  SEA SALIR	(4)
- * 			LEER DATOS 
  * 			CALCULAR NOTAS
+ * 			LEER Y VALIDAR DATO (3)
  * 		FIN MIENTRAS
  * 		
  * 		MOSTRAR RESULTADOS
@@ -72,7 +92,12 @@ package EjerciciosIterativos;
  * 			SUMAR DATO
  * 			CONTADOR MAS 1
  * 			MENSAJE 2
- * 			LEER DATO
+ * 			LEER  DATO
+ * 			MIENTRAS (DATO SEA MENOR QUE -1 O MAYOR QUE 10)
+ * 				MENSAJE 3
+ * 				MENSAJE 2
+ * 				LEER DATO
+ * 			FIN MIENTRAS
  * 		FIN MIENTRAS
  * 		
  * 		MOSTRAR NOTA MENOR
@@ -95,4 +120,80 @@ package EjerciciosIterativos;
 
 public class NotaMediaAltayAlumnos {
 
-}
+	
+	public static void main(String[]args){
+	 
+		double nota=0,notaAlta=0, notaBaja=10/*inicializo a 10, porque si es a 0 ninguna nota sera mas baja*/,sumadorNota=0, media=0;
+		int alumnos=0;
+		char ejecutar;
+		Scanner teclado=new Scanner(System.in);
+		// PREGUNTAR Y VALIDAR EJECUTAR (1)
+		
+		System.out.println("¿Desea ejecutar el programa?");
+		ejecutar=Character.toUpperCase(teclado.next().charAt(0));
+		
+		while(ejecutar!='N' && ejecutar!='S' ){
+			System.out.println("Introduzca 'S' o 'N'");
+			System.out.println("¿Desea ejecutar el programa?");
+			ejecutar=Character.toUpperCase(teclado.next().charAt(0));
+		}
+	  //	MIENTRAS EJECUTAR SEA SI	(2)	
+		while(ejecutar=='S'){
+	  //		LEER Y VALIDAR DATO		(3)
+			System.out.println("Introduzca una nota (utilize -1 para salir)");
+			nota=teclado.nextDouble();
+			while(nota<-1 || nota>10){
+				
+				System.out.println("La nota no es correcta el rango es de 0 a 10 ");
+				System.out.println("Introduzca una nota (utilize -1 para salir)");
+				nota=teclado.nextDouble();
+				
+			}
+	  //		MIENTRAS DATO NO  SEA SALIR	(4)
+			while(nota!=-1){
+	  
+	  //			CALCULAR NOTAS
+				if(nota!=-1){
+					
+					if(nota>notaAlta)
+						notaAlta=nota;
+					if(nota<notaBaja)
+						notaBaja=nota;
+					alumnos++;
+					sumadorNota+=nota;
+					media=(double)sumadorNota/alumnos;
+				}
+			
+//			LEER Y VALIDAR DATOS
+				System.out.println("Introduzca una nota (utilize -1 para salir)");
+				nota=teclado.nextDouble();
+				while(nota<-1 || nota>10){
+					
+					System.out.println("La nota no es correcta el rango es de 0 a 10 ");
+					System.out.println("Introduzca una nota (utilize -1 para salir)");
+					nota=teclado.nextDouble();
+					
+				}
+			}
+	  		
+	  // 	MOSTRAR RESULTADOS
+			System.out.println("El numero de alumnos es: "+alumnos);
+			System.out.println("La nota mas alta es: "+notaAlta);
+			System.out.println("La nota mas baja es: "+notaBaja);
+			System.out.println("La media de todas las notas es: "+media);
+			
+			
+	  //	ACTUALIZAR Y VALIDAR EJECUTAR (1)
+			System.out.println("¿Desea ejecutar el programa?");
+			ejecutar=Character.toUpperCase(teclado.next().charAt(0));
+			
+			while(ejecutar!='N' && ejecutar!='S' ){
+				System.out.println("Introduzca 'S' o 'N'");
+				System.out.println("¿Desea ejecutar el programa?");
+				ejecutar=Character.toUpperCase(teclado.next().charAt(0));
+			}//FIN VALIDAR
+		}	//	FIN MIENTRAS
+	  
+	}//FIN
+	
+}//FIN DE LA CLASE

cb22ed3fa8e0a75bcb4a4375f67ce5e4724faf50
Alexmaister <aortizurita@gmail.com>
2016-11-08 23:13:32 +0100

    Analisis trileros completo

--- PROGRAMAS/Tema3/Azar/Trileros.java
+++ PROGRAMAS/Tema3/Azar/Trileros.java
@@ -10,7 +10,27 @@ package Azar;
  * de que la bola este en su interior y se mostrara el resultado en pantalla, tambien se tendra 
  * la oportunidad de apostar una cantidad de dinero que se doblara en caso de acierto.
  * El trilero tendra en cuenta a cual cubilete se apuesta para intentar que el jugador no gane su apuesta
+ * 
+ * ENTRADAS:
+ * opcion de menu
+ * apuesta
+ * cubilete elegido
+ * 
+ * SALIDAS:
+ * menu (por pantalla)
+ * saldo	(por pantalla)
+ * resultado del cubilete (por pantalla)
+ * 
+ * RESTRICCIONES:
+ * La apuesta no podra ser mayor que el saldo
+ * La opcion elegida debe estar en el menu
+ * Solo se podra elegir entre 3 cubiletes , uno por tirada
+ * 
+ * SUPOSICIONES:
+ * Ninguna
+ * 
  * */
 public class Trileros {
 
+	
 }

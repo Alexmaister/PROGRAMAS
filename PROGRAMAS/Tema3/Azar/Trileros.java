77d872457be76cbea4a00d44451dc645d4eacc3d
Alexmaister <aortizurita@gmail.com>
2016-11-08 13:02:33 +0100

    Trileros-Analisis

---
+++ PROGRAMAS/Tema3/Azar/Trileros.java
@@ -0,0 +1,16 @@
+package Azar;
+/* Diseña un algoritmo para simular el juego de ¿dónde está la bolita? (trile),
+ *  famoso timo practicado por los llamados trileros .
+ *  En nuestro caso el ordenador será un honrado trilero 
+ *  que no engañará al usuario que juegue con él.*/
+
+/*ANALISIS:
+ * Programa que mostrara por pantalla la opcion de elegir entre tres cubiletes
+ * el cual uno de ellos contrendra una bola, cada cubilete tendra la misma probabilidad 
+ * de que la bola este en su interior y se mostrara el resultado en pantalla, tambien se tendra 
+ * la oportunidad de apostar una cantidad de dinero que se doblara en caso de acierto.
+ * El trilero tendra en cuenta a cual cubilete se apuesta para intentar que el jugador no gane su apuesta
+ * */
+public class Trileros {
+
+}

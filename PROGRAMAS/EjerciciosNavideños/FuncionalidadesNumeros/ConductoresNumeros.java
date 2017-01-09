edf4cbcdb4c15aa830288f25f52cf30c812037ee
Alexmaister <aortizurita@gmail.com>
2017-01-10 00:56:29 +0100

    terminado numeroPerfecto recursivo

--- PROGRAMAS/EjerciciosNavideños/FuncionalidadesNumeros/ConductoresNumeros.java
+++ PROGRAMAS/EjerciciosNavideños/FuncionalidadesNumeros/ConductoresNumeros.java
@@ -5,6 +5,6 @@ public class ConductoresNumeros {
 	public static void main(String[] args) {
 			
 		System.out.println(Numeros.numeroCompuesto(294));
-		//System.out.println(Numeros.numPerfecto(8128));
+		System.out.println(Numeros.numPerfecto(496));
 	}
 }

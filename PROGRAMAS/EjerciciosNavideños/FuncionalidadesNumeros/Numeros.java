03f1a164a0998ba27d96bbd86774bb3c8ee3fb99
Alexmaister <aortizurita@gmail.com>
2017-01-10 16:23:38 +0100

    error

--- PROGRAMAS/EjerciciosNavideños/FuncionalidadesNumeros/Numeros.java
+++ PROGRAMAS/EjerciciosNavideños/FuncionalidadesNumeros/Numeros.java
@@ -18,7 +18,64 @@ public class Numeros {
 		return (acumulador>2)?true:false;
 	}
 	
-	/*
-	 * 
+	/*cabecera: public static boolean numPerfecto(int num)
+	 * descripcion: funcion que comprobara si un numero es perfecto
+	 * entradas:un entero 
+	 * precondiciones: que el numero no sea negativo 
+	 * salidas : un boleano
+	 * postcondiciones: el resultado sera true cuando el numero sea perfecto y false cuando no
 	 */
+	
+	/*public static boolean numPerfecto(int num){
+		
+		return (num==num-Math.floor(num))?true:false;
+	}
+	public static int enteroSuperior(double num){
+		int decimal=0;
+		decimal=
+		if()
+		return int 
+	}*///intento 1
+	
+	public static boolean numPerfecto(int num){
+		boolean resultado;
+		
+		if((num%2.0==0.0) && (num==SumaDivisoresNumPerfecto(num-(int)Math.round(num/2.0))))
+			resultado=true;
+		else resultado=false;
+		 
+
+		return resultado;
+		
+		//return(num==SumaDivisoresNumPerfecto(num-(int)Math.round(num/2.0)))?true:false;
+
+	}
+public static int SumaDivisoresNumPerfecto(int a){
+	int acumulador=0;
+	if(a!=1){
+		acumulador=a+ SumaDivisoresNumPerfecto((int)Math.round(a/2.0));
+	}else
+		acumulador=1;
+	return acumulador;
+} //intento 4,5,...
+	
+	
+	/*cabecera: public static int siguienteDivisorPerf(int n)
+	 * descripcion: funcion que calculara el siguiente divisor de un numero
+	 * entradas:un entero
+	 * precondiciones: el numero debe ser positivo y divisor de un numero perfecto 
+	 * salidas: un entero
+	 * postcondiciones: el resultado sera un numero divisor siguiente mas pequeño del numero dado
+	 *
+	
+	public static int siguienteDivisorPerf(int n){
+		
+		int resultado=0;
+		
+		
+		return resultado;
+	}*/
+	
+
+	
 }
